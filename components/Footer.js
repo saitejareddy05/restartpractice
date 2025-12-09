cfec89f8d8cb88a48dc089f15ac294d902ceab4e
Kasireddy Saiteja Reddy <152090678+saitejareddy05@users.noreply.github.com>
2025-12-10 01:20:13 +0530

    Delete components directory

--- components/Footer.js
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-return (
-<footer style={{ marginTop: "40px", padding: "10px", background: "#f5f5f5" }}>
-<p>© 2025 My Next.js App</p>
-</footer>
-);
-}
--- components/Navbar.js
+++
@@ -1,9 +0,0 @@
-export default function Navbar() {
-return (
-<nav style={{ padding: "10px", background: "#eee" }}>
-<a href="/" style={{ marginRight: "15px" }}>Home</a>
-<a href="/about" style={{ marginRight: "15px" }}>About</a>
-<a href="/blog">Blog</a>
-</nav>
-);
-}
